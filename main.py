119521011fd66ebd4bea4a7f3081d30c2133ec4c
azimmerman409 <126191581+azimmerman409@users.noreply.github.com>
2024-04-16 16:15:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relev
 alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('relev--ant','-ele-phant'), ('AAAGAATTCA', 'AAA---T-CA')]
 
 def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
-    if (S == ""):
-        return(len(T))
-    elif (T == ""):
-        return(len(S))
+    if S == "":
+        return len(T)
+    elif T == "":
+        return len(S)
     else:
-        if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
+        if S[0] == T[0]:
+            return MED(S[1:], T[1:])
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+            return 1 + min(MED(S, T[1:]), MED(S[1:], T), MED(S[1:], T[1:]))
+
 
 
 def fast_MED(S, T, MED={}):
